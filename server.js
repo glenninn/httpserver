9613f081a78994372f03fe22ba2080aad500ae37
Glenn Inn <glenninn@yahoo.com>
2018-04-06 12:04:01 -0700

    Add redisplay of server session
    after displaying multiple request lots, redisplay server session info
    occasion

--- server.js
+++ server.js
@@ -8,22 +8,35 @@ var ourPath = path.dirname( process.argv[1] );
 var htmlDir = process.argv[2] || "html";
 var port = 80;
 var secPort = process.argv[3] || 443;
+var lc = 0;
+
 
 app.use(express.static(htmlDir),function(req,res,next){
-	console.log("(http) Req: "+req.path);
+	conMsg("(http) Req: "+req.path);
 	next();
 });
+
+
+function sessionStats() {
+console.log("\n***************************************************");
+console.log("HTTPS listening on port: " + secPort);
+console.log("HTTP listening on port: " + port);
+console.log("HTML files folder: " + htmlDir);
+console.log("*****************************************************");
+}
 
 console.log("\n************** HTTPS Server **************");
 console.log("Usage: node server.js {html folder}  {port}");
 console.log("  Create simple secure http Web server that answers on the specified TCP port (default: 80)");
 console.log("  and serves web pages from the specified html folder {default: ./html");
-console.log("\n***************************************************");
-console.log("HTTPS listening on port: " + secPort);
-console.log("HTTP listening on port: " + port);
-console.log("HTML files folder: " + htmlDir);
-console.log("*\n***************************************************");
-console.log("\n");
+sessionStats();
+
+function conMsg(s){
+	console.log(s);
+	lc++;
+	if( (lc % 20) == 0 )
+		sessionStats();
+}
 
 
 var sslOptions = {
@@ -33,17 +46,17 @@ var sslOptions = {
 var httpsServer = https.createServer(sslOptions, app);
 
 var server = httpsServer.listen(secPort, function () {
-    console.log('HTTPS Server listening at port %d', secPort);
+    conMsg('HTTPS Server listening at port '+ secPort);
 });
 
 server.on('connection',function(cobj){
-	console.log("(Https) connection: " + JSON.stringify(cobj.address()) );
+	conMsg("(Https) connection: " + JSON.stringify(cobj.address()) );
 });
 
 
 
 app.listen(80,function(){
-	console.log("HTTP Server listening at port %d", port);
+	conMsg("HTTP Server listening at port " + port);
 });
 
 
